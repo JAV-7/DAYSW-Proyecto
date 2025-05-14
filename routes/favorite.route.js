415380a7a45af8017f419d1816e1308720ad06d7
Javier Ramos <javier.ramos@iteso.mx>
2025-05-14 13:20:03 -0600

    Swagger doc

--- routes/favorite.route.js
+++ routes/favorite.route.js
@@ -1,7 +1,71 @@
+// favorite.route
 const express = require('express');
 const router = express.Router();
 const controllerFavorite = require('../controllers/favorite.controller');
 const { verifyToken } = require('../middleware/auth.middleware');
+
+/**
+ * @swagger
+ * tags:
+ *   name: Favorites
+ *   description: Favorite pets
+ */
+
+/**
+ * @swagger
+ * /favorites:
+ *   get:
+ *     summary: Get all favorite pets for the current user
+ *     tags: [Favorites]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of favorites
+ */
+
+/**
+ * @swagger
+ * /favorites:
+ *   post:
+ *     summary: Add a pet to favorites
+ *     tags: [Favorites]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               pet:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Favorite added
+ */
+
+/**
+ * @swagger
+ * /favorites/{petId}:
+ *   delete:
+ *     summary: Remove a pet from favorites
+ *     tags: [Favorites]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: petId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Favorite removed
+ */
+
+
 
 // Add a pet to favorites
 router.post('/', verifyToken, controllerFavorite.addFavorite);
--- routes/pet.route.js
+++ routes/pet.route.js
@@ -6,6 +6,83 @@ const { authorizeRole } = require('../middleware/roles.middleware');
 const { verifyToken } = require('../middleware/auth.middleware');
 const upload = require('../middleware/upload');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Pets
+ *   description: Pet management
+ */
+
+/**
+ * @swagger
+ * /pets:
+ *   get:
+ *     summary: Get all pets
+ *     tags: [Pets]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of pets
+ */
+
+/**
+ * @swagger
+ * /pets/{id}:
+ *   get:
+ *     summary: Get pet by ID
+ *     tags: [Pets]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pet retrieved
+ */
+
+/**
+ * @swagger
+ * /pets/species/{species}:
+ *   get:
+ *     summary: Get pets by species
+ *     tags: [Pets]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: species
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pets by species
+ */
+
+/**
+ * @swagger
+ * /pets/breed/{breed}:
+ *   get:
+ *     summary: Get pets by breed
+ *     tags: [Pets]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: breed
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pets by breed
+ */
+
 // PUBLIC ROUTES
 router.get('/', verifyToken, petController.getPets);
 router.get('/:id', verifyToken, petController.getPetById);
